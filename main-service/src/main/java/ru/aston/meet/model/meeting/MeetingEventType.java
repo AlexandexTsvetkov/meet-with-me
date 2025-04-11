08130f674b8d6ecff471b7876e228cd05e97fa96
Alexander Tsvetkov <finter124@yandex.ru>
2025-04-11 16:02:59 +0300

    Added remind events

--- main-service/src/main/java/ru/aston/meet/model/meeting/MeetingEventType.java
+++ main-service/src/main/java/ru/aston/meet/model/meeting/MeetingEventType.java
@@ -1,10 +1,7 @@
 package ru.aston.meet.model.meeting;
 
 import org.apache.avro.specific.SpecificRecordBase;
-import ru.aston.meet.kafka.notifications.meeting.CreateMeetingAvro;
-import ru.aston.meet.kafka.notifications.meeting.DeleteMeetingAvro;
-import ru.aston.meet.kafka.notifications.meeting.EditMeetingAvro;
-import ru.aston.meet.kafka.notifications.meeting.InvitedUser;
+import ru.aston.meet.kafka.notifications.meeting.*;
 import ru.aston.meet.model.user.User;
 
 import java.time.ZoneId;
@@ -38,6 +35,24 @@ public enum MeetingEventType {
         }
     },
     EDIT {
+        @Override
+        public SpecificRecordBase mapToMeetEventSpecificRecordBase(Meeting meeting, List<User> users) {
+
+            User initiator = meeting.getInitiator();
+
+            return RemindMeetingAvro.newBuilder()
+                    .setDescription(meeting.getDescription())
+                    .setEventDate(meeting.getEventDate().atZone(ZoneId.systemDefault())
+                            .toInstant())
+                    .setTitle(meeting.getTitle())
+                    .setLocation(meeting.getLocation())
+                    .setInitiatorName(initiator.getName())
+                    .setInitiatorEmail(initiator.getEmail())
+                    .setInvited(users.stream().map(user -> InvitedUser.newBuilder().setEmail(user.getEmail()).setName(user.getName()).build()).collect(Collectors.toList()))
+                    .build();
+        }
+    },
+    REMIND {
         @Override
         public SpecificRecordBase mapToMeetEventSpecificRecordBase(Meeting meeting, List<User> users) {
 
